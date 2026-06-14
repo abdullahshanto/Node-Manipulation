bf70f315580a84a07539e6554a50b08ee8c2fe8b
abdullahshanto <abdurrahman7328@gmail.com>
2026-06-14 16:22:03 +0600

    Delete practice directory

--- practice/calculator/app.js
+++
@@ -1,9 +0,0 @@
-import *as http from 'node:http';
-
-const server = http.createServer()
-
-const port=3000;
-server.listen(port,()=>
-{
-  console.log(`server listening http://localhost:${port}`);
-})
--- practice/calculator/sum.js
+++
@@ -1,4 +0,0 @@
-const sum=(res,req)=>
-{
-  
-}
